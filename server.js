26ebb82396b42345f281239c4718b7351f1cfe81
fatihkaya13 <56218812+fatihkaya13@users.noreply.github.com>
2021-12-02 15:31:47 +0300

    adding bearer token auth middleware

--- server.js
+++ server.js
@@ -1,28 +1,52 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bearerToken = require('express-bearer-token');
 const db = require('./data/db');
+
+// instantiate express application
 const app = express();
 
+// define public key for API
+const PUBLIC_KEY = '589b00de-9062-4fd3-8e01-6d3a49d877d1';
+
+// define port
 const PORT = process.env.PORT || 3000;
 
+// parse json body for POST requests
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// instantiate bearer token as first middleware
+app.use(bearerToken());
+
+// continue middleware with
+app.use(function (req, res, next) {
+  if(req.token == PUBLIC_KEY){
+    next()
+  }else{
+    res.status(401).json({
+      success: false,
+      message: `Not Authorized for this API with ${req.token ? 'token: ' + req.token: 'no token'}`,
+    });
+  }
+});
+
+// TODO: bütün res.send res.json olarak güncellenecek
 app.get('/', (req, res) => {
-    res.send('api is ready to receive requests. please use /inventory endpoint');
+    res.json({msg: 'api is ready to receive requests. please use /inventory endpoint'});
 });
 
 app.get('/inventory', (req, res) => {
-    res.send({inventory: db});
+    res.json({inventory: db});
 });
 
 app.get('/inventory/:id', (req, res) => {
     const {id} = req.params;
     const item = db.find( item => item.id === Number(id) )
     if(item){
-      res.status(200).send(item);
+      res.status(200).json(item);
     } else {
-      res.status(404).send('item not found');
+      res.status(404).json({msg: `Item not found with specified id: ${id}`});
     }
 });
 
@@ -34,9 +58,9 @@ app.put('/inventory/:id', (req, res) => {
     if (item) {
       item.quantity = quantity;
       item.price = price;
-      res.send(db[itemIndex]);
+      res.json(db[itemIndex]);
     } else {
-      res.status(404).send();
+      res.status(400).json({msg: 'Cannot handle PUT request'});
     }
 });
 
@@ -48,14 +72,16 @@ app.patch('/inventory/:id', (req, res) => {
     if (item) {
       item.quantity = quantity;
       item.price = price;
-      res.send(db[itemIndex]);
+      res.json(db[itemIndex]);
     } else {
-      res.status(404).send();
+      res.status(400).send({msg: 'Cannot handle PATCH request'});
     }
 });
 
 // TODO: CHECK OBJECT KEYS, CATEGORIES, VALID PRICES ETC
 app.post('/inventory', (req, res, next) => {
+    console.log(req.body)
+    // TODO: body boş olsa da ekliyor
     if (req.body) {
         const {item, category, quantity, price} = req.body;
         let lastID = db[db.length-1].id;
@@ -72,9 +98,9 @@ app.post('/inventory', (req, res, next) => {
             "price": price
         }
         db.push(newItem);
-        res.status(201).send(newItem);
+        res.status(201).json(newItem);
     } else {
-      res.status(400).send();
+      res.status(400).json({msg: 'Cannot handle post request'});
     }
 });
 
@@ -84,13 +110,15 @@ app.delete('/inventory/:id', (req, res, next) => {
     if (itemIndex !== -1) {
       db.splice(itemIndex, 1);
       // TODO:: what info to send?
-      res.status(200).send();
+      res.status(200).json({msg: `Item with id ${id} is deleted`});
     } else {
-      res.status(404).send();
+      res.status(400).json({msg: `Cannot handle delete request with specified id: ${id}`});
     }
 });
-  
+
 
 app.listen(PORT, () => {
   console.log(`Server is listening on ${PORT}`);
 });
+
+// curl -X GET http://localhost:3000/ -H "Authorization: Bearer 589b00de-9062-4fd3-8e01-6d3a49d877d1"
