80f566e9d6aaa41656067dac993c44698d47f5ec
Fatih Kaya <fatihkaya13@gmail.com>
2021-12-04 12:39:13 +0300

    prettier format

--- data/db.js
+++ data/db.js
@@ -1,123 +1,144 @@
-const db = [{
-    "id": 1,
-    "name": "Tea",
-    "category": "Drinks",
-    "quantity": 615,
-    "price": 5
-  }, {
-    "id": 2,
-    "name": "Energy drink",
-    "category": "Drinks",
-    "quantity": 403,
-    "price": 12
-  }, {
-    "id": 3,
-    "name": "Bag Stand",
-    "category": "Tools",
-    "quantity": 338,
-    "price": 30
-  }, {
-    "id": 4,
-    "name": "Macaroons",
-    "category": "Sweets",
-    "quantity": 193,
-    "price": 12.21
-  }, {
-    "id": 5,
-    "name": "Salmon fish",
-    "category": "Food",
-    "quantity": 98,
-    "price": 90
-  }, {
-    "id": 6,
-    "name": "Red lipstick",
-    "category": "Beauty",
-    "quantity": 456,
-    "price": 169,
-  }, {
-    "id": 7,
-    "name": "Parmesan Cheese",
-    "category": "Food",
-    "quantity": 674,
-    "price": 78,
-  }, {
-    "id": 8,
-    "name": "Banana",
-    "category": "Food",
-    "quantity": 577,
-    "price": 24,
-  }, {
-    "id": 9,
-    "name": "Coke, 500 Ml",
-    "category": "Drinks",
-    "quantity": 234,
-    "price": 5,
-  }, {
-    "id": 10,
-    "name": "Diater",
-    "category": "Baby",
-    "quantity": 960,
-    "price": 65,
-  }, {
-    "id": 11,
-    "name": "Cheddar Cheese",
-    "category": "Food",
-    "quantity": 832,
-    "price": 76,
-  }, {
-    "id": 12,
-    "name": "Oil - Cooking Spray",
-    "category": "Tools",
-    "quantity": 94,
-    "price": 98,
-  }, {
-    "id": 13,
-    "name": "Lamb - Whole, Fresh",
-    "category": "Food",
-    "quantity": 280,
-    "price": 120,
-  }, {
-    "id": 14,
-    "name": "Car wash pump",
-    "category": "Industrial",
-    "quantity": 926,
-    "price": 187,
-  }, {
-    "id": 15,
-    "name": "Rice - Jasmine",
-    "category": "Food",
-    "quantity": 421,
-    "price": 41
-  }, {
-    "id": 16,
-    "name": "Gold bracelet",
-    "category": "Jewelry",
-    "quantity": 2,
-    "price": 599
-  }, {
-    "id": 17,
-    "name": "Lamb - Whole Head Off",
-    "category": "Food",
-    "quantity": 637,
-    "price": 221
-  }, {
-    "id": 18,
-    "name": "Cookies - Fortune",
-    "category": "Sweets",
-    "quantity": 835,
-    "price": 4
-  }, {
-    "id": 19,
-    "name": "Pepper - Roasted Red",
-    "category": "Food",
-    "quantity": 637,
-    "price": 1
-  }, {
-    "id": 20,
-    "name": "Cat food",
-    "category": "Pets",
-    "quantity": 159,
-    "price": 149
-  }];
+const db = [
+  {
+    id: 1,
+    name: "Tea",
+    category: "Drinks",
+    quantity: 615,
+    price: 5,
+  },
+  {
+    id: 2,
+    name: "Energy drink",
+    category: "Drinks",
+    quantity: 403,
+    price: 12,
+  },
+  {
+    id: 3,
+    name: "Bag Stand",
+    category: "Tools",
+    quantity: 338,
+    price: 30,
+  },
+  {
+    id: 4,
+    name: "Macaroons",
+    category: "Sweets",
+    quantity: 193,
+    price: 12.21,
+  },
+  {
+    id: 5,
+    name: "Salmon fish",
+    category: "Food",
+    quantity: 98,
+    price: 90,
+  },
+  {
+    id: 6,
+    name: "Red lipstick",
+    category: "Beauty",
+    quantity: 456,
+    price: 169,
+  },
+  {
+    id: 7,
+    name: "Parmesan Cheese",
+    category: "Food",
+    quantity: 674,
+    price: 78,
+  },
+  {
+    id: 8,
+    name: "Banana",
+    category: "Food",
+    quantity: 577,
+    price: 24,
+  },
+  {
+    id: 9,
+    name: "Coke, 500 Ml",
+    category: "Drinks",
+    quantity: 234,
+    price: 5,
+  },
+  {
+    id: 10,
+    name: "Diater",
+    category: "Baby",
+    quantity: 960,
+    price: 65,
+  },
+  {
+    id: 11,
+    name: "Cheddar Cheese",
+    category: "Food",
+    quantity: 832,
+    price: 76,
+  },
+  {
+    id: 12,
+    name: "Oil - Cooking Spray",
+    category: "Tools",
+    quantity: 94,
+    price: 98,
+  },
+  {
+    id: 13,
+    name: "Lamb - Whole, Fresh",
+    category: "Food",
+    quantity: 280,
+    price: 120,
+  },
+  {
+    id: 14,
+    name: "Car wash pump",
+    category: "Industrial",
+    quantity: 926,
+    price: 187,
+  },
+  {
+    id: 15,
+    name: "Rice - Jasmine",
+    category: "Food",
+    quantity: 421,
+    price: 41,
+  },
+  {
+    id: 16,
+    name: "Gold bracelet",
+    category: "Jewelry",
+    quantity: 2,
+    price: 599,
+  },
+  {
+    id: 17,
+    name: "Lamb - Whole Head Off",
+    category: "Food",
+    quantity: 637,
+    price: 221,
+  },
+  {
+    id: 18,
+    name: "Cookies - Fortune",
+    category: "Sweets",
+    quantity: 835,
+    price: 4,
+  },
+  {
+    id: 19,
+    name: "Pepper - Roasted Red",
+    category: "Food",
+    quantity: 637,
+    price: 1,
+  },
+  {
+    id: 20,
+    name: "Cat food",
+    category: "Pets",
+    quantity: 159,
+    price: 149,
+  },
+];
 
-  module.exports = db;
+module.exports = db;
